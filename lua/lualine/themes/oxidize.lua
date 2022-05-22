8260babe98d4313092c6a4719c89e20eef51de23
Ryan Turner <zdbiohazard2@gmail.com>
2022-05-21 21:44:26 -0700

    Generate lualine colors from highlight groups
    This will allow for dynamic color changes later

--- lua/lualine/themes/oxidize.lua
+++ lua/lualine/themes/oxidize.lua
@@ -3,17 +3,35 @@
 -- Author: Ryan Turner <zdbiohazard2@gmail.com>
 -- Source: https://github.com/ZDBioHazard/vim-oxidize
 
-return {
+local function lookup(group)
     -- We're using cterm colors here to let lualine generate the RGB colors
     -- lualine will leave out the cterm colors entirely if we use RGB here
+    local color = vim.api.nvim_get_hl_by_name(group, false)
+    return { fg = color.foreground, bg = color.background }
+end
+
+local a = {
+    text     = lookup('SignColumn').bg,
+    normal   = lookup('StatusLine').fg,
+    insert   = lookup('Statement').fg,
+    replace  = lookup('Number').fg,
+    visual   = lookup('Identifier').fg,
+    command  = lookup('PreProc').fg,
+    inactive = lookup('StatusLineNC').fg,
+}
+
+return {
     normal = {
-        a = {fg = 233, bg = 178, gui = 'bold'},
-        b = {fg = 250, bg = 233},
-        c = {fg = 178, bg = 235},
+        a = {fg = a.text, bg = a.normal, gui = 'bold'},
+        b = lookup('SignColumn'),
+        c = lookup('StatusLine'),
     },
-    insert   = {a = {fg = 233, bg = 208, gui = 'bold'}},
-    replace  = {a = {fg = 233, bg = 166, gui = 'bold'}},
-    visual   = {a = {fg = 233, bg =  31, gui = 'bold'}},
-    command  = {a = {fg = 233, bg =  96, gui = 'bold'}},
-    inactive = {a = {fg = 250, bg = 240, gui = 'bold'}},
+    insert   = {a = {fg = a.text, bg = a.insert, gui = 'bold'}},
+    replace  = {a = {fg = a.text, bg = a.replace, gui = 'bold'}},
+    visual   = {a = {fg = a.text, bg = a.visual, gui = 'bold'}},
+    command  = {a = {fg = a.text, bg = a.command, gui = 'bold'}},
+    inactive = {
+        a = {fg = a.text, bg = a.inactive, gui = 'bold'},
+        c = lookup('StatusLineNC'),
+    },
 }
